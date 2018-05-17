221c3fbde651972a20bf9e8923176b4345c584bb
John Barker <jebarker@gmail.com>
2018-05-17 15:44:16 -0400

    Monitor processes in the chainlink container and die if 1 dies

--- chainlink-launcher.sh
+++ chainlink-launcher.sh
@@ -1,3 +1,28 @@
 #!/bin/bash -e
+
+waitForResponse () {
+  sleepCount=0
+  while [ "$sleepCount" -le "10" ] && ! curl -s "$1"; do
+      sleep 1
+      sleepCount=$((sleepCount+1))
+  done
+
+  if [ "$sleepCount" -gt "10" ]; then
+    exit 1
+  fi
+}
+
+trap "kill -- -$$ || true" SIGINT SIGTERM EXIT
+
 /opt/intel/sgxpsw/aesm/aesm_service &
-./chainlink $@
+aesm_pid=$!
+
+waitForResponse http://geth:18546
+
+./chainlink $@ &
+chainlink_pid=$!
+
+while sleep 30; do
+  kill -0 $aesm_pid
+  kill -0 $chainlink_pid
+done
