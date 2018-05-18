95d2c15bcacbc99e4c5d496e63399ead23d464d1
John Barker <jebarker@gmail.com>
2018-05-18 10:43:10 -0400

    Don't wait for AESM if in simulation mode

--- chainlink-launcher.sh
+++ chainlink-launcher.sh
@@ -23,6 +23,8 @@ waitForResponse http://geth:18546
 chainlink_pid=$!
 
 while sleep 30; do
-  kill -0 $aesm_pid
+  if [ ! -eq $SGX_SIMULATION yes ]; then
+    kill -0 $aesm_pid
+  fi
   kill -0 $chainlink_pid
 done
